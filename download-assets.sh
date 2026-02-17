50e7fc60feadc4fc27ce95498a91caa0b1c3d878
Unizori <orest7857@gmail.com>
2026-02-17 11:09:04 -0600

    Update download-assets.sh

--- download-assets.sh
+++ download-assets.sh
@@ -1,7 +1,7 @@
 @echo off
 echo Downloading Unity WebGL build...
 
-mkdir Build
+if not exist Build mkdir Build
 
 curl -L "https://github.com/Unizori/My-personal-card/releases/download/1.0.0/1675bcd6b2cb7bd32a74efc4a176e13c.data.unityweb" -o Build/1675bcd6b2cb7bd32a74efc4a176e13c.data.unityweb
 curl -L "https://github.com/Unizori/My-personal-card/releases/download/1.0.0/51c6f8ca36b10639222676083d520732.data.unityweb" -o Build/51c6f8ca36b10639222676083d520732.data.unityweb
