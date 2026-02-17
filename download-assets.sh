8ca72b25a851921faa3515e3d8396c74608a352b
Unizori <orest7857@gmail.com>
2026-02-16 18:13:02 -0600

    download file

--- download-assets.sh
+++ download-assets.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # download-assets.sh
 
-mkdir -p assets
 
 # Download from your cloud storage
-curl -L "https://drive.google.com/file/d/1TKsxTRwKhCOtdX4QOlQAp4u0kwsWUMLY/view?usp=sharing" -o Build
-curl -L "https://drive.google.com/uc?export=download&id=FILE_ID_2" -o Build
+curl -L "https://drive.google.com/file/d/1TKsxTRwKhCOtdX4QOlQAp4u0kwsWUMLY/view?usp=sharing" -o assets/Build.data.unityweb
+curl -L "https://drive.google.com/file/d/1nIPejdfviZ_CIjwN3Wgh8UXp7DRARyM0/view?usp=sharing" -o assets2/Build.data.unityweb
 
 echo "Assets downloaded!"
